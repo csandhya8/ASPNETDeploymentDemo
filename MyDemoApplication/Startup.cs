bb846ced717ba7260a4e902125b3ac6c46241b9f
Tim Reilly <timreilly@live.com>
2017-02-01 12:52:47 -0800

    initial commit

---
+++ MyDemoApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyDemoApplication.Startup))]
+namespace MyDemoApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
